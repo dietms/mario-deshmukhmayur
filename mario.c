4714c766d61686dd9ac48f930b65125cb99127f5
deshmukhmayur <deshmukhmayur@outlook.com>
2016-08-31 23:42:39 +0530

    Changed the program style to Functional programming.

--- mario.c
+++ mario.c
@@ -1,16 +1,24 @@
+//CS50 PSet 1-C: Mario
 #include<stdio.h>
 
+void triangle(int n)                                                          {
+  if (n>1 && n<23)                                                            {
+    for (int i=2; i < (n+2); i++)                                             {
+      for (int j=0; j < (n+1-i); j++)                                         {
+        printf(" ")                                                           ;
+                                                                              }
+      for (int j=1; j <= i; j++)                                              {
+        printf("#")                                                           ;
+                                                                              }
+      printf("\n")                                                            ;
+                                                                              }
+                                                                              }
+                                                                              }
+
 int main()                                                                    {
-  int n                                                                       ;
+  int height                                                                  ;
   printf("Height: ")                                                          ;
-  scanf("%d", &n)                                                             ;
-  for (int i=2; i < (n+2); i++)                                               {
-    for (int j=1; j < (n+1-i); j++)                                           {
-      printf(" ")                                                             ;
-                                                                              }
-    for (int j=1; j <= i; j++)                                                {
-      printf("#")                                                             ;
-                                                                              }
-    printf("\n")                                                              ;
-                                                                              }
+  scanf("%d", &height)                                                        ;
+  triangle(height)                                                            ;
+  return 0                                                                    ;
                                                                               }
