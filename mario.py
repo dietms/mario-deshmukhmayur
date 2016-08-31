c66cb8c22bca6b68a2f033898421b0585b82b2af
deshmukhmayur <deshmukhmayur@outlook.com>
2016-08-31 21:49:04 +0530

    Refined the validation for n <= 23, according to the original question given on CS50

--- mario.py
+++ mario.py
@@ -1,6 +1,6 @@
 '''CS50 PSet 1-C: Mario'''
 def main(n):
-    if n >= 1:
+    if n >= 1 and n <= 23:
         for i in range(2,n+2):
             print(' '*(n+1-i), end='')
             print('#'*i)
