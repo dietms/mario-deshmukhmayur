653d1b04325f9d21393bcda4a07b00f058f0d45d
Mayur Deshmukh <deshmukhmayur@outlook.com>
2016-08-31 22:22:35 +0530

    Update mario.cpp

--- mario.cpp
+++ mario.cpp
@@ -4,7 +4,7 @@ using namespace std                                                           ;
 int main() {
   int n                                                                       ;
   cout << "Height: "                                                          ;
-  cin >> n                                                                  ;
+  cin >> n                                                                    ;
   for (int i=2; i < (n+2); i++)                                               {
     for (int j=1; j < (n+1-i); j++)                                           {
       cout << " "                                                             ;
