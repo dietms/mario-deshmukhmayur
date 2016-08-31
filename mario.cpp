b0c094087b74c59fc79939d2eb141dc3ead2780d
deshmukhmayur <deshmukhmayur@outlook.com>
2016-08-31 23:44:16 +0530

    Changed the program style to Object Oriented.

--- mario.cpp
+++ mario.cpp
@@ -1,17 +1,32 @@
+//CS50 PSet 1-C: Mario
 #include "iostream"
-using namespace std                                                           ;
+using namespace std;
+
+class Mario {
+  int height;
+
+public:
+  void triangle() {
+    if (this->height>1 && this->height<23) {
+      for (int i=2; i<(this->height+2); i++) {
+        for (int j=0; j<(this->height+1-i); j++) {
+          cout << " ";
+        }
+        for (int j=1; j<=i; j++) {
+          cout << "#";
+        }
+        cout << endl;
+      }
+    }
+  }
+  void getHeight() {
+    cout << "Height: ";
+    cin >> this->height;
+  }
+};
 
 int main() {
-  int n                                                                       ;
-  cout << "Height: "                                                          ;
-  cin >> n                                                                    ;
-  for (int i=2; i < (n+2); i++)                                               {
-    for (int j=1; j < (n+1-i); j++)                                           {
-      cout << " "                                                             ;
-                                                                              }
-    for (int j=1; j <= i; j++)                                                {
-      cout << "#"                                                             ;
-                                                                              }
-    cout << "\n"                                                              ;
-                                                                              }
-                                                                              }
+  Mario m;
+  m.getHeight();
+  m.triangle();
+}
